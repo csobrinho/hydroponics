08224e2a34bc82e3783eb336f2db9d5ad497fa6d
Carlos Sobrinho <sobrinho@google.com>
2019-12-17 03:20:28 +0000

    Fixed the network module initialization code.

--- main/network/wifi.c
+++ main/network/wifi.c
@@ -20,40 +20,49 @@ static int s_retry_num = 0;
 
 static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        ESP_LOGI(TAG, "WIFI_EVENT_STA_START");
+        ESP_LOGD(TAG, "WIFI_EVENT_STA_START");
         esp_wifi_connect();
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        ESP_LOGI(TAG, "WIFI_EVENT_STA_DISCONNECTED");
+        ESP_LOGD(TAG, "WIFI_EVENT_STA_DISCONNECTED");
         if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
             esp_wifi_connect();
             xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
             s_retry_num++;
             ESP_LOGI(TAG, "retry to connect to the AP");
         }
-        ESP_LOGI(TAG, "connect to the AP fail");
+        ESP_LOGW(TAG, "connect to the AP fail");
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ESP_LOGI(TAG, "IP_EVENT_STA_GOT_IP");
+        ESP_LOGD(TAG, "IP_EVENT_STA_GOT_IP");
         ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
-        ESP_LOGI(TAG, "got ip:"
-                IPSTR, IP2STR(&event->ip_info.ip));
+        ESP_LOGI(TAG, "received ip: "
+                IPSTR
+                " gw: "
+                IPSTR, IP2STR(&event->ip_info.ip), IP2STR(&event->ip_info.gw));
         s_retry_num = 0;
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     } else {
-        ESP_LOGI(TAG, "EVENT_ID:%d", event_id);
+        ESP_LOGD(TAG, "EVENT_ID:%d", event_id);
     }
 }
 
 esp_err_t wifi_init(const char *ssid, const char *password) {
-    s_wifi_event_group = xEventGroupCreate();
+    esp_log_level_set("wifi", ESP_LOG_WARN);
+    esp_log_level_set("esp_netif_handlers", ESP_LOG_WARN);
 
     ESP_ERROR_CHECK(esp_netif_init());
+    s_wifi_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
 
     wifi_config_t wifi_config = {
             .sta = {
@@ -64,14 +73,13 @@ esp_err_t wifi_init(const char *ssid, const char *password) {
     };
     if (ssid != NULL && password != NULL) {
         strlcpy((char *) wifi_config.sta.ssid, ssid, sizeof(wifi_config.sta.ssid));
-        strlcpy((char *) wifi_config.sta.password, ssid, sizeof(wifi_config.sta.password));
+        strlcpy((char *) wifi_config.sta.password, password, sizeof(wifi_config.sta.password));
     }
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_connect());
 
-    ESP_LOGI(TAG, "Connecting to SSID:%s password:%s", wifi_config.sta.ssid, wifi_config.sta.password);
-
+    ESP_LOGI(TAG, "Connecting to SSID: '%s'", wifi_config.sta.ssid);
     return ESP_OK;
 }
