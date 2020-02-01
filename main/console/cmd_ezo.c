fe9adc58c3295957d771c6cb88f35562e4cb4057
Carlos Sobrinho <sobrinho@google.com>
2020-01-31 19:21:22 -0800

    Added the probe's water RTD sensor value to the console command and make sure we lock before running the command.

--- main/console/cmd_ezo.c
+++ main/console/cmd_ezo.c
@@ -2,6 +2,7 @@
 #include <strings.h>
 
 #include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
 
 #include "esp_err.h"
 #include "esp_console.h"
@@ -11,6 +12,7 @@
 #include "driver/ezo.h"
 #include "sensors/ezo_ec.h"
 #include "sensors/ezo_ph.h"
+#include "sensors/ezo_rtd.h"
 
 static struct {
     struct arg_str *module;
@@ -21,10 +23,11 @@ static struct {
 } cmd_args;
 
 static ezo_sensor_t *find_sensor(const char *name) {
-    if (strncasecmp(name, ezo_ec->type, strlen(name)) == 0) {
-        return ezo_ec;
-    } else if (strncasecmp(name, ezo_ph->type, strlen(name)) == 0) {
-        return ezo_ph;
+    ezo_sensor_t *probes[] = {ezo_ec, ezo_ph, ezo_rtd};
+    for (int i = 0; i < sizeof(probes) / sizeof(ezo_sensor_t *); ++i) {
+        if (strncasecmp(name, probes[i]->type, strlen(name)) == 0) {
+            return probes[i];
+        }
     }
     return NULL;
 }
@@ -40,17 +43,19 @@ static int action(int argc, char **argv) {
         printf("Unknown module named '%s'\n", cmd_args.module->sval[0]);
         return ESP_ERR_INVALID_ARG;
     }
+    xSemaphoreTake(sensor->lock, portMAX_DELAY);
     esp_err_t err = ezo_send_command(sensor, cmd_args.delay->ival[0], "%s", cmd_args.cmd->sval[0]);
     if (err == ESP_OK) {
         char buf[EZO_MAX_BUFFER_LEN] = {0};
         err = ezo_parse_response(sensor, 1, "%s", buf);
         printf("Status: %d '%s'\n", sensor->status, buf);
     }
+    xSemaphoreGive(sensor->lock);
     return err;
 }
 
 esp_err_t cmd_ezo_init(void) {
-    cmd_args.module = arg_str1(NULL, NULL, "<ec|ph>", "ezo module name");
+    cmd_args.module = arg_str1(NULL, NULL, "<ec|ph|rtd>", "ezo module name");
     cmd_args.address = arg_int1(NULL, NULL, "<addr>", "i2c address");
     cmd_args.delay = arg_int1(NULL, NULL, "<ms>", "delay in ms to wait");
     cmd_args.cmd = arg_str1(NULL, NULL, "<cmd>", "command to send");
