dd0e633346310cec8b6ba139c8c2cdcedc851514
Carlos Sobrinho <sobrinho@google.com>
2020-05-08 17:46:07 -0700

    Moved all into the header.

--- main/embedded.c
+++
@@ -1,4 +0,0 @@
-#include "embedded.h"
-
-const uint16_t HYDROPONICS_LOGO_BIN_WIDTH = 126;
-const uint16_t HYDROPONICS_LOGO_BIN_HEIGHT = 69;
--- main/embedded.h
+++ main/embedded.h
@@ -3,9 +3,9 @@
 
 #include <stdint.h>
 
+#define HYDROPONICS_LOGO_BIN_WIDTH 126
+#define HYDROPONICS_LOGO_BIN_HEIGHT 69
 extern const uint8_t HYDROPONICS_LOGO_BIN_START[] asm("_binary_hydroponics_logo_bin_start");
 extern const uint8_t HYDROPONICS_LOGO_BIN_END[] asm("_binary_hydroponics_logo_bin_end");
-extern const uint16_t HYDROPONICS_LOGO_BIN_WIDTH;
-extern const uint16_t HYDROPONICS_LOGO_BIN_HEIGHT;
 
 #endif //HYDROPONICS_EMBEDDED_H
