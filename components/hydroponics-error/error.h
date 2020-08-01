c0eeb04693b49ceaddd501a4d685db4343daec29
Carlos Sobrinho <sobrinho@google.com>
2020-07-31 23:20:41 -0700

    Added a few more helper macros to the 'error' module.

--- components/hydroponics-error/error.h
+++ components/hydroponics-error/error.h
@@ -11,22 +11,36 @@
 #define ARG_UNUSED(x) (void)(x)
 
 #define ARG_CHECK(a, str, ...) do {                                                         \
-      if(!(a)) {                                                                            \
+      if (!(a)) {                                                                           \
         arg_loge(TAG, "%s:%d (%s): " str, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
         return ESP_ERR_INVALID_ARG;                                                         \
       }                                                                                     \
     } while(0)
 
+#define CHECK_NO_MEM(ptr) do {                                                              \
+      if ((ptr) == NULL) {                                                                  \
+        arg_loge(TAG, "%s:%d (%s): out of memory", __FILE__, __LINE__, __FUNCTION__);       \
+        return ESP_ERR_NO_MEM;                                                              \
+      }                                                                                     \
+    } while(0)
+
 #define ARG_ERROR_CHECK(a, str, ...) do {                                                   \
-      if(!(a)) {                                                                            \
+      if (!(a)) {                                                                           \
         arg_loge(TAG, "%s:%d (%s): " str, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
         ESP_ERROR_CHECK(ESP_ERR_INVALID_STATE);                                             \
       }                                                                                     \
     } while(0)
 
 #define FAIL_IF(a, str, ...) do {                                                           \
-      if((a)) {                                                                             \
+      if ((a)) {                                                                            \
         arg_loge(TAG, "%s:%d (%s): " str, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
+        goto fail;                                                                          \
+      }                                                                                     \
+    } while(0)
+
+#define FAIL_IF_NO_MEM(ptr) do {                                                            \
+      if ((ptr) == NULL) {                                                                  \
+        arg_loge(TAG, "%s:%d (%s): out of memory", __FILE__, __LINE__, __FUNCTION__);       \
         goto fail;                                                                          \
       }                                                                                     \
     } while(0)
