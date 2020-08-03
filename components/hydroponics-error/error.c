cae89eb6a749b91fc4aa164ca1071ac287dfdd24
Carlos Sobrinho <sobrinho@google.com>
2020-08-02 23:59:09 -0700

    Make sure we only print the right amount of chars (although vasprintf null terminates the string any way).

--- components/hydroponics-error/error.c
+++ components/hydroponics-error/error.c
@@ -8,9 +8,9 @@ void arg_loge(const char *tag, const char *fmt, ...) {
     va_list va;
     va_start(va, fmt);
     char *buf = NULL;
-    vasprintf(&buf, fmt, va);
+    int len = vasprintf(&buf, fmt, va);
     if (buf != NULL) {
-        ESP_LOGE(tag, "%s", buf);
+        ESP_LOGE(tag, "%.*s", len, buf);
         free(buf);
     }
     va_end(va);
