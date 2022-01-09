9bf4bbbad74fba3187e2d1739d121d7d2a11eb6b
Carlos Sobrinho <sobrinho@google.com>
2022-01-09 01:05:06 -0800

    Log an error if the NVS was in an unexpected state.

--- main/storage.c
+++ main/storage.c
@@ -14,7 +14,7 @@ esp_err_t storage_init(context_t *context) {
 
     esp_err_t err = nvs_flash_init();
     if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        // NVS partition was truncated and needs to be erased. Retry nvs_flash_init.
+        ESP_LOGE(TAG, "NVS partition was truncated and needs to be erased. err: %s", esp_err_to_name(err));
         ESP_ERROR_CHECK(nvs_flash_erase());
         err = nvs_flash_init();
     }
